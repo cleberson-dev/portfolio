c3bac14c634da41028af438e91836b1332662a2d
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2020-05-28 00:13:58 -0300

    Alterado Componente Section (exportando o Container)

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,7 +1,1 @@
-import React from 'react';
-
-import { Container } from './styles';
-
-const Section = ({ children }) => <Container>{children}</Container>;
-
-export default Section;
+export { Container as default } from './styles';
