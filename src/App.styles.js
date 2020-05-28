9a0492630b2e427d1a331d747765c62219c49f11
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2020-05-27 23:32:36 -0300

    Criado componente 'Section'

--- src/App.styles.js
+++ src/App.styles.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 const AppContainer = styled.div`
-  background-color: #282c34;
   min-height: 100vh;
 `;
 
---
+++ src/components/Section/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+  width: 99vw;
+  min-height: 100vh;
+  box-sizing: border-box;
+`;
