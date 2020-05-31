c75cf42dce7d67e2c5d8cb4f3c7fd088f740de45
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2020-05-30 21:06:32 -0300

    Refatorado Seção 'Sobre'

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import githubIcon from './img/github.svg';
 import twitterIcon from './img/twitter.svg';
 import emailIcon from './img/at.svg';
 
+import profilePhoto from './img/profile-photo.jpg';
+
 
 
 function App() {
@@ -30,7 +32,16 @@ function App() {
         ]}
       />
 
-      <AboutSection />
+      <AboutSection 
+        description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac ligula placerat, egestas lacus sed, molestie urna. Vivamus volutpat venenatis nunc tincidunt eleifend. Aenean ac venenatis risus. Sed nec finibus felis, in vestibulum magna. Vivamus vulputate volutpat augue at aliquet. Nullam vel maximus lacus. Etiam in blandit lorem. Nullam euismod tempus augue et posuere. In sit amet fermentum magna, sit amet semper tellus. Donec efficitur scelerisque metus vitae congue. Ut vel metus quis ex dictum pulvinar. Vestibulum pretium sapien vel velit dignissim tincidunt. Etiam ullamcorper ex eget sapien aliquam malesuada. Mauris rhoncus arcu sapien, sed cursus felis dapibus id. Phasellus vitae arcu lobortis, feugiat risus in, pellentesque lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac ligula placerat, egestas lacus sed, molestie urna. Vivamus volutpat venenatis nunc tincidunt eleifend. Aenean ac venenatis risus. Sed nec finibus felis, in vestibulum magna. Vivamus vulputate volutpat augue at aliquet. Nullam vel maximus lacus. Etiam in blandit lorem. Nullam euismod tempus augue et posuere. In sit amet fermentum magna, sit amet semper tellus. Donec efficitur scelerisque metus vitae congue. Ut vel metus quis ex dictum pulvinar. Vestibulum pretium sapien vel velit dignissim tincidunt. Etiam ullamcorper ex eget sapien aliquam malesuada. Mauris rhoncus arcu sapien, sed cursus felis dapibus id. Phasellus vitae arcu lobortis, feugiat risus in, pellentesque lectus.'
+        profilePhoto={profilePhoto}
+        summary={{
+          name: 'Cleberson F R Junior',
+          birth: [11, 1, 1999],
+          location: 'São Luís - MA, Brasil',
+          education: 'Engenharia da Computação (7/10)'
+        }}
+      />
     </AppContainer>
   );
 }
--- src/sections/About/index.js
+++ src/sections/About/index.js
@@ -2,31 +2,28 @@ import React from 'react';
 
 import Title from '../../components/Title';
 
-import profilePhoto from '../../img/profile-photo.jpg';
+import { getAge } from '../../utils/math';
 
 import { 
   Container, Description, 
   Me, Summary, SummaryText, ProfilePhoto 
 } from './styles';
 
-function About() {
+function About({ description, profilePhoto, summary }) {
+  const [day, month, year] = summary.birth;
+  const age = getAge([day, month, year]);
+
   return (
     <Container as="section">
       <Title>🧑 Sobre mim</Title>
-      <Description>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac ligula placerat, egestas lacus sed, molestie urna. Vivamus volutpat venenatis nunc tincidunt eleifend. Aenean ac venenatis risus. Sed nec finibus felis, in vestibulum magna. 
-        Vivamus vulputate volutpat augue at aliquet. Nullam vel maximus lacus. Etiam in blandit lorem. Nullam euismod tempus augue et posuere. In sit amet fermentum magna, sit amet semper tellus. Donec efficitur scelerisque metus vitae congue. Ut vel metus quis ex dictum pulvinar. Vestibulum pretium sapien vel velit dignissim tincidunt. Etiam ullamcorper ex eget sapien aliquam malesuada. Mauris rhoncus arcu sapien, sed cursus felis dapibus id. Phasellus vitae arcu lobortis, feugiat risus in, pellentesque lectus.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac ligula placerat, egestas lacus sed, molestie urna. Vivamus volutpat venenatis nunc tincidunt eleifend. Aenean ac venenatis risus. Sed nec finibus felis, in vestibulum magna. 
-        Vivamus vulputate volutpat augue at aliquet. Nullam vel maximus lacus. Etiam in blandit lorem. Nullam euismod tempus augue et posuere. In sit amet fermentum magna, sit amet semper tellus. Donec efficitur scelerisque metus vitae congue. Ut vel metus quis ex dictum pulvinar. Vestibulum pretium sapien vel velit dignissim tincidunt. Etiam ullamcorper ex eget sapien aliquam malesuada. Mauris rhoncus arcu sapien, sed cursus felis dapibus id. Phasellus vitae arcu lobortis, feugiat risus in, pellentesque lectus.
-      </Description>
-      
+      <Description>{description}</Description>
       <Me>
-        <ProfilePhoto src={profilePhoto} alt="Arrr" />
+        <ProfilePhoto src={profilePhoto} alt="Foto de Perfil" />
         <Summary>
-          <SummaryText>Cleberson F R Junior</SummaryText>
-          <SummaryText>21 anos</SummaryText>
-          <SummaryText>São Luís - MA, Brasil</SummaryText>
-          <SummaryText>Engenharia da Computação (7/10)</SummaryText>
+          <SummaryText>{summary.name}</SummaryText>
+          <SummaryText>{age} anos</SummaryText>
+          <SummaryText>{summary.location}</SummaryText>
+          <SummaryText>{summary.education}</SummaryText>
         </Summary>
       </Me>
     </Container>
